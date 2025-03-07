498702190b236e5e46301f11a25e16165cb258df
niketanmoon <niketanmoon@gmail.com>
2025-03-07 13:53:48 +1030

    Added landing page

--- README.md
+++ README.md
@@ -17,3 +17,17 @@
 - Add clerkMiddleware
 - add protected route using createRouteMatcher: https://clerk.com/docs/references/nextjs/custom-sign-in-or-up-page
 - Add Clerk Provider to use this middleware in your layout.tsx just above the html tag
+
+## Shadcn setup
+
+- Install using `npx shadcn@latest init`
+- Add button component: `npx shadcn@latest add button`
+
+## Continue to building app
+
+- remove everything on page.tsx except div with classname as empty
+- Start building the home screen by modifying page.tsx
+- Added scrolling on page.tsx and added min height screen and height screen to layout.tsx
+- Add image domain to next config
+- Added constants file to utils folder
+- Built the landing page
---
+++ src/utils/constant.ts
@@ -0,0 +1,48 @@
+import {
+  BrainCogIcon,
+  EyeIcon,
+  GlobeIcon,
+  MonitorSmartphoneIcon,
+  ServerCogIcon,
+  ZapIcon,
+} from "lucide-react";
+
+export const features = [
+  {
+    name: "Store your PDF Documents",
+    description:
+      "Keep all your important PDF files securely stored and easily accessible anytime, anywhere.",
+    icon: GlobeIcon,
+  },
+  {
+    name: "Blazing Fast Responses",
+    description:
+      "Experience lightning-fast answers to your queries, ensuring you get the information you need instantly.",
+    icon: ZapIcon,
+  },
+  {
+    name: "Chat Memorisation",
+    description:
+      "Our intelligent chatbot remembers previous interactions, providing a seamless and personalized experience.",
+    icon: BrainCogIcon,
+  },
+  {
+    name: "Interactive PDF Viewer",
+    description:
+      "Engage with your PDFs like never before using our intuitive and interactive viewer.",
+    icon: EyeIcon,
+  },
+  {
+    name: "Cloud Backup",
+    description:
+      "Rest assured knowing your documents are safely backed up on the cloud, protected from loss or damage.",
+    icon: ServerCogIcon,
+  },
+  {
+    name: "Responsive Across Devices",
+    description:
+      "Access and chat with your PDFs seamlessly on any device, whether it's your desktop, tablet, or smartphone.",
+
+    icon: MonitorSmartphoneIcon,
+  },
+];
